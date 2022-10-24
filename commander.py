173530086802e44a785fc5014a619e6462fc6193
Maxim <vedernikovmaxim@rambler.ru>
2022-10-24 21:16:36 +0300

    added internet search

--- commander.py
+++ commander.py
@@ -2,6 +2,8 @@ import os
 import speech_recognition
 import pyttsx3
 from random import choice
+from serpapi import GoogleSearch
+
 
 sr = speech_recognition.Recognizer()
 sr.pause_threshold = 0.5
@@ -82,8 +84,19 @@ def play_sound():
 #######################
 
 def internet_search():
-    pass
+    answer_function('Что хотите найти?')
+    phrase = listen_command()
+    parameter = {
+        "engine": "yandex",
+        "text": phrase,
+        "api_key": "1a15ef08c83fb7bba62201558cd28c125d1eccab2a77a808e84ed7aa55f20d3f"
+    }
 
+    search = GoogleSearch(parameter)
+    dict_results = search.get_dict()
+
+    print(dict_results['organic_results'][0]['link'])
+    print(dict_results['organic_results'][0]['snippet'])
 
 
 def main():
--- main.py
+++ main.py
@@ -2,49 +2,18 @@ from serpapi import GoogleSearch
 
 
 def main():
-    start = 0
-    end = 40
-    page_size = 10
 
-    # basic search parameters
     parameter = {
-        "q": "coca cola",
-        "tbm": "nws",
-        "api_key": "1a15ef08c83fb7bba62201558cd28c125d1eccab2a77a808e84ed7aa55f20d3f",
-        # optional pagination parameter
-        #  the pagination method can take argument directly
-        "start": start,
-        "end": end,
-        "num": page_size
+        "engine": "yandex",
+        "text": "что такое бой",
+        "api_key": "1a15ef08c83fb7bba62201558cd28c125d1eccab2a77a808e84ed7aa55f20d3f"
     }
 
-    # as proof of concept
-    # urls collects
-    urls = []
-
-    # initialize a search
     search = GoogleSearch(parameter)
+    dict_results = search.get_dict()
 
-    # create a python generator using parameter
-    pages = search.pagination()
-    # or set custom parameter
-    pages = search.pagination(start, end, page_size)
-
-    # fetch one search result per iteration
-    # using a basic python for loop
-    # which invokes python iterator under the hood.
-    for page in pages:
-        print(f"Current page: {page['serpapi_pagination']['current']}")
-        for news_result in page["news_results"]:
-            print(f"Title: {news_result['title']}\nLink: {news_result['link']}\n")
-            urls.append(news_result['link'])
-
-    # check if the total number pages is as expected
-    # note: the exact number if variable depending on the search engine backend
-    if len(urls) == (end - start):
-        print("all search results count match!")
-    if len(urls) == len(set(urls)):
-        print("all search results are unique!")
+    print(dict_results['organic_results'][0]['link'])
+    print(dict_results['organic_results'][0]['snippet'])
 
 
 if __name__ == '__main__':
